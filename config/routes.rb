5895ffd69611fcf2791f94bdb703f613e7711017
mfq <softmfq@gmail.com>
2016-11-04 20:20:17 +0500

    added route Constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
   resources :pages, only: [:new]
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'pages#new', as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: 'pages#index'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
